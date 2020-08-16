2a031fba37a10586b9761f878e603f4c75640d93
Mark Nagy <mark.nagy@etnetera.eu>
2020-08-16 11:05:54 +0200

    basic nav graph implementation

---
+++ app/src/main/java/com/teakave/beeping/presentation/feature/OverviewFragment.kt
@@ -0,0 +1,18 @@
+package com.teakave.beeping.presentation.feature
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.teakave.beeping.R
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.overview_fragment, container, false)
+
+}
